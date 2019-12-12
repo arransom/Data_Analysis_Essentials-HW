d6b63d1e5f2edfd2f1e79d47f2c2426578b38220
Amy <arransom@gmail.com>
2019-12-12 00:07:16 -0800

    # Added third input variable - z_col, to allow user to map data by color
    # Removed default dot and line colors

--- AmyR_L05-HW_Functions_for_Exploratory_Viz.R
+++ AmyR_L05-HW_Functions_for_Exploratory_Viz.R
@@ -9,17 +9,16 @@ library(tidyverse) # tidyverse
 # Write Function to create consistent plots ------------------------------------
 
 # Define input variables and default parameters 
-gg_explore <- function(data, x_col, y_col, 
+gg_explore <- function(data, x_col, y_col, z_col,
                        param_title = str_c(y_col, " by ", x_col),
                        param_subtitle = str_c("Row count: ", data %>% nrow()),
                        param_alpha = 0.5,
                        param_method = "lm",
                        param_se = FALSE, 
-                       param_dot_color = "blue",
-                       param_line_color = "orange") {
+                       param_line_color = "purple") {
   # Generate an exploratory plot and save it to the environment as "plt"
-  plt <- data %>% ggplot(mapping = aes_string(x = x_col, y = y_col)) +
-    geom_jitter(alpha = param_alpha, color = param_dot_color) + 
+  plt <- data %>% ggplot(mapping = aes_string(x = x_col, y = y_col, color = z_col)) +
+    geom_jitter(alpha = param_alpha) + 
     geom_smooth(method = param_method, se = param_se, color = param_line_color) +
     ggtitle(param_title, subtitle = param_subtitle) 
   # Display the Plot
@@ -27,4 +26,8 @@ gg_explore <- function(data, x_col, y_col,
 }
 
 # Test the function using the mtcars dataset
-gg_explore(mtcars, "disp", "mpg")
+gg_explore(df_mtcars, "disp", "mpg", "cyl")
+glimpse(mtcars)
+df_mtcars <- mtcars %>%
+  mutate(cyl = as.factor(cyl),
+         gear = as.factor(gear))
